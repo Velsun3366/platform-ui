f7ad2a44f8feaca90bf12f5bdb6d34baf53b07d6
Elvea Huang <elvea.huang@gmail.com>
2022-09-18 18:41:44 +0800

    一直在整理，一直在重构，不是在整理就是在重构。

--- platform-ui-react/update.sh
+++ platform-ui-react/update.sh
@@ -2,32 +2,32 @@
 # commons
 cd ./packages/commons
 rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
 # mobile
 cd ../../packages/mobile
 rm -rf package-lock.json
-rm -rf node_modules
-ncu -u
-# mobile
-cd ../../packages/mp
-rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
 # wap
 cd ../../packages/wap
 rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
 # webapp
 cd ../../packages/webapp
 rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
 # root
 cd ../../
 rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
 #
-npm install
+pnpm install
